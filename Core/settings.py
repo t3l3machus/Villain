318ea0b65fdaf68e6ae9fc6f284d66cbc7a8e6f1
Panagiotis Chartas <t3l3machus@protonmail.com>
2022-10-26 23:04:42 +0300

    Update settings.py

--- Core/settings.py
+++ Core/settings.py
@@ -10,11 +10,24 @@ class Threading_params:
 
 class Core_server_settings:
 	
+	bind_address = '0.0.0.0'
+	bind_port = 65001	
 	# How long to sleep between echo requests to check if siblings are alive
 	ping_siblings_sleep_time = 5 
 
 
 
+class Hoaxshell_settings:
+	
+	bind_address = '0.0.0.0'
+	bind_port = 65002
+	_header = 'Authorization' # Header name of the header that carries the rshell's SID
+	default_frequency = 0.8   # Beacon loop frequency of the rshells
+	certfile = False # Add path to cert.pem here for SSL
+	keyfile = False  # Add path to priv_key.pem here for SSL
+
+
+
 class Sessions_manager_settings:
 	
-	shell_state_change_after = 5.0 
+	shell_state_change_after = 4.0 
