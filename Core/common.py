67f6c6c6278996f328414498d86f902aaf902422
Panagiotis Chartas <t3l3machus@protonmail.com>
2024-09-15 10:22:32 +0200

    Update common.py

--- Core/common.py
+++ Core/common.py
@@ -6,7 +6,7 @@
 # https://github.com/t3l3machus/Villain
 
 
-import sys, string, base64, os, re, traceback
+import sys, string, base64, os, re, traceback, socket
 import netifaces as ni
 from random import randint, choice, randrange
 from threading import Thread, enumerate as enumerate_threads
@@ -60,7 +60,6 @@ META = '[\001\033[38;5;93m\002M\001\033[38;5;129m\002e\001\033[38;5;165m\002t\00
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-
 ''' Command Prompt Settings '''
 
 class Main_prompt:
@@ -111,7 +110,7 @@ def print_debug(msg):
 
 
 
-def chill():
+def do_nothing():
 	pass
 
 
@@ -319,42 +318,388 @@ def print_columns(strings):
 
 
 
-def validate_host_address(addr):
-
-	addr_verified = False
-	try:
-		# Check if valid IP address
-		#re.search('[\d]{1,3}[\.][\d]{1,3}[\.][\d]{1,3}[\.][\d]{1,3}', lhost_value)
-		addr_verified = str(ip_address(addr))
-
-	except ValueError:
-
-		try:
-			# Check if valid interface
-			addr_verified = ni.ifaddresses(addr)[ni.AF_INET][0]['addr']
-
-		except:
-			# Check if valid hostname
-			if len(addr) > 255:
-				addr_verified = False
-				print('Hostname length greater than 255 characters.')
-				return False
-			
-			if addr[-1] == ".":
-				addr = addr[:-1]  # Strip trailing dot (used to indicate an absolute domain name and technically valid according to DNS standards)
-
-			disallowed = re.compile(r"[^A-Z\d-]", re.IGNORECASE)
-			if all(len(part) and not part.startswith("-") and not part.endswith("-") and not disallowed.search(part) for part in addr.split(".")):
-				# Check if hostname is resolvable
-				try:
-					socket.gethostbyname(addr)
-					addr_verified = addr
-				
-				except:
-					print('Failed to resolve LHOST.')			
-			
-	return addr_verified
+class PrompHelp:
 	
+	commands = {
+	
+		'connect' : {
+			'details' : f''' 			
+Connect with another instance of Villain (sibling server). Once connected, you will be able to see and interact with foreign shell sessions owned by sibling servers and vice-versa. Multiple sibling servers can be connected at once. The limit of connections depends on the number of active threads a Villain instance can have (adjustable). In case you forgot the team server port number (default: 6501), use "sockets" to list Villain related services info. Read the Usage Guide or check my YouTube channel (@HaxorTechTones) for details.
+
+connect <IP> <TEAM_SERVER_PORT>''',
+			'least_args' : 2,
+			'max_args' : 2
+		},
+				
+				
+		'generate' : {
+			'details' : f''' 		
+Generate a reverse shell command. This function has been redesigned to use payload templates, which you can find in Villain/Core/payload_templates and edit or create your own.
+
+Main logic:
+generate payload=<OS_TYPE/HANDLER/PAYLOAD_TEMPLATE> lhost=<IP or INTERFACE> [ obfuscate encode ]
+
+Usage examples:
+generate payload=windows/netcat/powershell_reverse_tcp lhost=eth0 encode
+generate payload=linux/hoaxshell/sh_curl lhost=eth0
+
+- The ENCODE and OBFUSCATE attributes are enabled for certain templates and can be used during payload generation. 
+- For info on a particular template, use "generate" with PAYLOAD being the only provided argument.
+- To catch HoaxShell https-based reverse shells you need to start Villain with SSL.
+- Ultimately, one should edit the templates and add obfuscated versions of the commands for AV evasion.''',
+			'least_args' : 0, # Intentionally set to 0 so that the Payload_Generator class can inform users about missing arguments
+			'max_args' : 7
+		},			
+
+
+		'exec' : {
+			'details' : f''' 			
+Execute a command or file against an active backdoor session. Files are executed by being http requested from the Http File Smuggler. The feature works regardless if the session is owned by you or a sibling server.
+	
+exec <COMMAND or LOCAL FILE PATH> <SESSION ID or ALIAS>
+
+*Command(s) should be quoted.''',
+			'least_args' : 2,
+			'max_args' : 2
+		},			
+
+
+		'inject' : {
+			'details' : f''' 			
+Execute a local file via fileless exec over http. The feature works regardless if the session is owned by you or a sibling server. You can only use it inside an active pseudo shell session.
+	
+inject <LOCAL FILE PATH> 
+
+''',
+			'least_args' : 1,
+			'max_args' : 1,
+			'shell' : True
+		},		
+
+		'repair' : {
+			'details' : f''' 			
+Use this command to manually correct a backdoor session's hostname/username value, in case Villain does not interpret the information correctly when the session is established.
+ 	
+repair <SESSION ID or ALIAS> <HOSTNAME or USERNAME> <NEW VALUE>''',
+			'least_args' : 3,
+			'max_args' : 3
+		},	
+			
+		'shell' : {
+			'details' : f''' 			
+Enables an interactive pseudo-shell prompt for a backdoor session. Press Ctrl+C to disable.
+ 
+shell <SESSION ID or ALIAS>''',
+			'least_args' : 1,
+			'max_args' : 1
+		},			
+
+			
+		'alias' : {
+			'details' : f'''
+Set an alias for a backdoor session to use instead of session ID.
+
+alias <ALIAS> <SESSION ID>''',
+			'least_args' : 2,
+			'max_args' : 2
+		},			
+
+			
+		'reset' : {
+			'details' : f'''
+Reset a given alias to the original session ID.
+
+reset <ALIAS>''',
+			'least_args' : 1,
+			'max_args' : 1
+		},			
+
+		
+		'kill' : {
+			'details' : f'''
+Terminate a self-owned backdoor session.
+
+kill <SESSION ID or ALIAS>''',
+			'least_args' : 1,
+			'max_args' : 1
+		},		
+
+		
+		'help' : {
+			'details' : f'''Really?''',
+			'least_args' : 0,
+			'max_args' : 1
+		},
+
+		'siblings' : {
+			'details' : f'''
+Print info about connected Sibling Servers. 
+Siblings are basically other instances of Villain that you are connected with.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'threads' : {
+			'details' : f'''
+Villain creates a lot of threads to be able to handle multiple backdoor sessions, connections with siblings and more. In file Villain/Core/settings.py there is a BoundedSemaphore that works as a thread limiter to prevent resource contention, set by default to 100 (you can of course change it). This command lists the active threads created by Villain, to give you an idea of what is happening in the background, what is the current value of the thread limiter etc.
+  
+Note that, if the thread limiter reaches 0, weird things will start happening as new threads (e.g. backdoor sessions) will be queued until: thread limiter > 0.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'sessions' : {
+			'details' : f'''Prints info about active backdoor shell sessions.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'backdoors' : {
+			'details' : f'''Prints specifics about the shell and listener types of active backdoor shell sessions.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'sockets' : {
+			'details' : f'''Prints Villain related socket services info.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'id' : {
+			'details' : f'''Print server's unique ID.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'upload' : {
+			'details' : f'''
+Upload files to a poisoned machine (files are auto-requested from the http file smuggler). The feature works regardless if the session is owned by you or a sibling server. You can only use it inside an active pseudo shell session.
+
+From an active pseudo shell prompt:
+upload <LOCAL_FILE_PATH> <REMOTE_FILE_PATH>''',
+			'least_args' : 3,
+			'max_args' : 3,
+			'shell' : True
+		},
+
+		'cmdinspector' : {
+			'details' : f'''
+Villain has a function that inspects user issued shell commands for input that may cause a backdoor shell session to hang (e.g., unclosed single/double quotes or backticks, commands that may start a new interactive session within the current shell and more). 
+Use the cmdinspector command to turn that feature on/off. 
+
+cmdinspector <ON / OFF>''',
+			'least_args' : 1,
+			'max_args' : 1
+		},
+
+		'conptyshell' : {
+			'details' : f'''
+Automatically runs Invoke-ConPtyShell against a session. A new terminal window with netcat listening will pop up (you need to have gnome-terminal installed) and the script will be executed on the target as a new process, spawning a fully interactive shell. Currently works only for powershell.exe sessions.
+
+Usage: 
+conptyshell <IP or INTERFACE> <PORT> <SESSION ID or ALIAS>''',
+			'least_args' : 3,
+			'max_args' : 3
+		},
+
+		'exit' : {
+			'details' : f'''Kill all self-owned sessions and quit.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'flee' : {
+			'details' : f'''Quit without terminating active sessions. When you start Villain again, if any HoaxShell implant is still running on previously injected hosts, the session(s) will be re-established.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'clear' : {
+			'details' : f'''Come on man.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'purge' : {
+			'details' : f'''Villain automatically stores information regarding generated implants and loads them in memory every time it starts. This way, HoaxShell generated implants become reusable and it is possible to re-establish older sessions, assuming the payload is still running on the victim(s). Use this command to delete all session related metadata. It does not affect any active sessions you may have.''',
+			'least_args' : 0,
+			'max_args' : 0
+		},
+
+		'redirectors' : {
+			'details' : f'''
+Villain instances set traffic redirectors when using a shell session that belongs to a sibling. Use this command to list or remove redirectors if required (setting redirectors is handled automatically by Villain when needed). This feature exists mostly for troubleshooting reasons.
+
+Usage: 
+Type "redirectors" to list all active redirector entries.
+
+To remove a redirector:
+redirectors pop <REDIRECTOR ID>
+''',
+			'least_args' : 0,
+			'max_args' : 2
+		},
+	}
+	
+	
+	@staticmethod
+	def print_main_help_msg():
+				
+		print(
+		f'''
+		\r  Main Prompt
+		\r  -----------
+		\r
+		\r  Command              Description
+		\r  -------              -----------
+		\r  help         [+]     Print this message.
+		\r  connect      [+]     Connect with a sibling server.
+		\r  generate     [+]     Generate backdoor payload.
+		\r  siblings             Print sibling servers data table.
+		\r  sessions             Print established backdoor sessions data table.
+		\r  backdoors            Print established backdoor types data table.
+		\r  sockets              Print Villain related running services' info.
+		\r  redirectors  [+]     List and manage traffic redirectors.
+		\r  shell        [+]     Enable an interactive pseudo-shell for a session.
+		\r  alias        [+]     Set an alias for a shell session.
+		\r  reset        [+]     Reset alias back to the session's unique ID.
+		\r  kill         [+]     Terminate an established backdoor session.
+		\r  conptyshell  [+]     Slap Invoke-ConPtyShell against a backdoor session.
+		\r  repair       [+]     Manually correct a session's hostname/username info.
+		\r  id                   Print server's unique ID (Self).
+		\r  cmdinspector [+]     Turn Session Defender on/off.
+		\r  threads              Print information regarding active threads.
+		\r  clear                Clear screen.
+		\r  purge                Delete all stored sessions metadata.
+		\r  flee                 Quit without terminating active sessions.
+		\r  exit                 Kill all sessions and quit.
+		\r  
+		\r
+		\r  Pseudo Shell
+		\r  ------------
+		\r  
+		\r  Command              Description
+		\r  -------              -----------
+		\r  upload       [+]     Upload files into an active shell session.
+		\r  inject       [+]     Fileless exec of local scripts over http.
+		\r
+		\r  Commands starting with "#" are interpreted as messages and will be 
+		\r  broadcasted to all connected Sibling Servers (chat).
+		\r
+        \r  Commands with [+] may require additional arguments.
+        \r  For details use: {ORANGE}help <COMMAND>{END}
+		''')
+			
+
+
+	@staticmethod
+	def print_detailed(cmd):			
+		PrompHelp.print_justified(PrompHelp.commands[cmd]['details'].strip()) if cmd in PrompHelp.commands.keys() \
+		else print(f'No details for command "{cmd}".')
+
+
+	@staticmethod
+	def print_justified(text):
+		
+		text_length = len(text)
+		text_lines = text.split('\n')
+		wrapped_text = ''
+		term_width = os.get_terminal_size().columns
+		term_width_p = 100 if 100 <= (term_width) else term_width - 2
+		
+		if text_length >= term_width_p:
+			
+			lines = []
+			
+			for p in text_lines:
+				
+				if len(p) <= (term_width_p - 3):
+					lines.append('  ' + p + ' ')
+					continue
+
+				words = p.split(' ')
+
+				if words == ['']:
+					continue
+
+				else:					
+					words_s = [w + ' ' for w in words]
+					line_length = 0
+					count = s = 0
+
+					for w in words_s:
+						
+						line_length += len(w)
+
+						if line_length < (term_width_p - 3):
+							count += 1
+
+						else:							
+							lines.append('  ' + ' '.join(words[s:count]) + ' ')
+							s = count
+							count += 1
+							line_length = len(w)
+					
+					if s < len(words):
+						lines.append(f'  ' + ' '.join(words[s:]) + f' ')
+				
+			wrapped_text = '\n'.join(lines)
+
+		else:
+			wrapped_text = text
+		
+		print('\n' + wrapped_text, end='\n\n')
+
+
+
+	@staticmethod
+	def validate(cmd, num_of_args):
+		
+		valid = True
+		
+		if cmd not in PrompHelp.commands.keys():
+			print('Unknown command.')
+			valid = False
+		
+		elif 'shell' in list(PrompHelp.commands[cmd].keys()):
+			print(f'You need to be inside a pseudo shell session to use "{cmd}". Type "help {cmd}" for more info.')
+			valid = False
+			
+		elif num_of_args < PrompHelp.commands[cmd]['least_args']:
+			print('Missing arguments.')
+			valid = False
+		
+		elif num_of_args > PrompHelp.commands[cmd]['max_args']:
+			print('Too many arguments. Use "help <COMMAND>" for details.')
+			valid = False			
+	
+		return valid
+
+
+
+''' Shell Naming Conventions '''
+
+bin2shell = {
+	'powershell.exe' : 'PowerShell',
+	'cmd.exe' : 'WIN_CMD',
+	'unix' : 'Unix',
+	'zsh' : 'Unix'
+	}
+
+
+bin2ext = {
+	'powershell.exe' : '.ps1',
+	'cmd.exe' : '.bat',
+	'unix' : '.sh',
+	'zsh' : '.sh'	
+}
+
+
+def multi_set(payload, set_dict):
+	for key,val in set_dict.items():
+		payload = payload.replace(key, str(val))
+	return payload
 
 
 ''' Encryption '''
@@ -380,3 +725,4 @@ def decrypt_msg(aes_key, cipher, iv):
 	
 	except TypeError:
 		pass
+
